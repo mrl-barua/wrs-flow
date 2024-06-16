1a8388d34e28569a18cb5d3ce20e8fb35b8480e8
Marlo Labisores Barua <104258357+mrl-barua@users.noreply.github.com>
2024-06-17 02:06:59 +0800

    fix the view router in which cannot redirect to other pages

--- src/router.ts
+++ src/router.ts
@@ -1,4 +1,4 @@
-import { createMemoryHistory, createRouter } from "vue-router";
+import { createWebHistory, createRouter } from "vue-router";
 
 import Login from "./views/Login.vue";
 import Home from "./views/Home.vue";
@@ -13,7 +13,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createMemoryHistory(),
+  history: createWebHistory(),
   routes,
 });
 
