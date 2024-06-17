12affe7fea8abf7dc703cbd4a4ae2e9dfe759582
Marlo Labisores Barua <104258357+mrl-barua@users.noreply.github.com>
2024-06-17 10:47:00 +0800

    chore: Update sidebar imports and remove unused code

--- src/components/sidebar.vue
+++ src/components/sidebar.vue
@@ -5,6 +5,7 @@ import "../js/core/popper.min.js";
 import "../js/core/bootstrap.min.js";
 
 // import "../js/plugin/jquery-scrollbar/jquery.scrollbar.min.js";
+import "../js/kaiadmin.min.js";
 import "../js/plugin/chart.js/chart.min.js";
 import "../js/plugin/jquery.sparkline/jquery.sparkline.min.js";
 // import "../js/plugin/chart-circle/circles.min.js";
@@ -13,23 +14,22 @@ import "../js/plugin/datatables/datatables.min.js";
 import "../js/plugin/jsvectormap/jsvectormap.min.js";
 import "../js/plugin/jsvectormap/world.js";
 // import "../js/plugin/sweetalert/sweetalert.min.js";
-import "../js/kaiadmin.min.js";
 
-WebFont.load({
-  google: { families: ["Public Sans:300,400,500,600,700"] },
-  custom: {
-    families: [
-      "Font Awesome 5 Solid",
-      "Font Awesome 5 Regular",
-      "Font Awesome 5 Brands",
-      "simple-line-icons",
-    ],
-    urls: ["assets/css/fonts.min.css"],
-  },
-  active: function () {
-    sessionStorage.fonts = true;
-  },
-});
+// WebFont.load({
+//   google: { families: ["Public Sans:300,400,500,600,700"] },
+//   custom: {
+//     families: [
+//       "Font Awesome 5 Solid",
+//       "Font Awesome 5 Regular",
+//       "Font Awesome 5 Brands",
+//       "simple-line-icons",
+//     ],
+//     urls: ["assets/css/fonts.min.css"],
+//   },
+//   active: function () {
+//     sessionStorage.fonts = true;
+//   },
+// });
 </script>
 
 <template>
